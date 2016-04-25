2d008dedb8f80d5dfc32c8ff752bcc7bb4618a3d
Marcus Bauer <mabako@gmail.com>
2016-04-25 21:06:27 +0200

    Problem #2

---
+++ euler002.erl
@@ -0,0 +1,17 @@
+-module(euler002).
+-author("mabako").
+
+-export([fibonacci/1]).
+
+fibonacci(UpperLimt) -> fibonacci_sum(0, UpperLimit, 1, 1).
+
+% Check if we're exceeding the upper limit imposed
+fibonacci_sum(Sum, UpperLimit, NumberMinus1, NumberMinus2) when NumberMinus1 + NumberMinus2 > UpperLimit -> Sum;
+
+% Is the new term even?
+fibonacci_sum(Sum, UpperLimit, NumberMinus1, NumberMinus2) when ((NumberMinus1 + NumberMinus2) rem 2) == 0 ->
+  Value = NumberMinus1 + NumberMinus2,
+  fibonacci_sum(Sum + Value, UpperLimit, Value, NumberMinus1);
+
+fibonacci_sum(Sum, UpperLimit, NumberMinus1, NumberMinus2) ->
+  fibonacci_sum(Sum, UpperLimit, NumberMinus1 + NumberMinus2, NumberMinus1).
