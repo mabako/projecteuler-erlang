557f9e254650dab2b0d4efef472157d517d50336
Marcus Bauer <mabako@gmail.com>
2016-04-26 23:54:17 +0200

    Problem #10

--- euler010.erl
+++ euler010.erl
@@ -1,13 +1,19 @@
-%%%-------------------------------------------------------------------
-%%% @author mabako
-%%% @copyright (C) 2016, <COMPANY>
-%%% @doc
-%%%
-%%% @end
-%%% Created : 26. Apr 2016 23:02
-%%%-------------------------------------------------------------------
+% After having used prime numbers for a while - namely, in problem #3 (prime factorization) and #7 (10001th prime),
+% let's finally generate a few primes using a variation on the sieve of Eratosthenes.
 -module(euler010).
--author("mabako").
+-export([sum_of_primes_up_to/1]).
 
-%% API
--export([]).
+sum_of_primes_up_to(N) ->
+  sum_of_primes_up_to(N, 2, lists:seq(3, N, 2)).
+
+sum_of_primes_up_to(_, Sum, []) -> Sum;
+
+% We only need to check up to sqrt(N) for removing numbers; once we've crossed this threshold all remaining numbers are
+% prime.
+sum_of_primes_up_to(N, Sum, [X | Numbers]) when X * X > N ->
+  sum_of_primes_up_to(N, X + Sum, Numbers);
+
+% Below the threshold of sqrt(N), cross out further primes.
+sum_of_primes_up_to(N, Sum, [X | Numbers]) ->
+  sum_of_primes_up_to(N, X + Sum, lists:filter(fun(Y) -> Y rem X /= 0 end, Numbers)).
+
