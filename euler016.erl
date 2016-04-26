5a54b1ae4637309c960dace9dbe5cd0fef6fd49d
Marcus Bauer <mabako@gmail.com>
2016-04-27 01:25:14 +0200

    Problem #20

--- euler016.erl
+++ euler016.erl
@@ -1,12 +1,10 @@
 -module(euler016).
--export([sum_of_digits/1,pow/2, solve/1]).
+-import(util, [sum_of_digits/1]).
+-export([pow/2, solve/1]).
 
 pow(Base, Exponent) -> pow(Base, Exponent, 1).
 
 pow(_, 0, Value) -> Value;
 pow(Base, Exponent, Value) -> pow(Base, Exponent - 1, Value * Base).
-
-sum_of_digits(N) ->
-  lists:sum(lists:map(fun(X) -> list_to_integer([X]) end, integer_to_list(N))).
 
 solve(N) -> sum_of_digits(pow(2, N)).
